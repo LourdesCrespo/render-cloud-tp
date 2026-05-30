fdca6516521ae319d61dec0bb9189809ace7ea29
nachom1cha <nachomicha06@gmail.com>
2026-05-30 10:59:23 -0300

    Update Worker.h

--- src/Worker.h
+++ src/Worker.h
@@ -1,9 +1,13 @@
 #ifndef WORKER_H
 #define WORKER_H
 
+#include <atomic>
 #include "MessageQueue.h"
 #include "PoolVRAM.h"
 
+// Permite al Worker conocer el estado de apagado controlado definido en el Main
+extern std::atomic<bool> sistemaActivo;
+
 void ejecutarWorker(int idWorker, MessageQueue& mq, PoolVRAM& pool, int cantidad);
 
-#endif// WORKER_H_INCLUDED
+#endif // WORKER_H
